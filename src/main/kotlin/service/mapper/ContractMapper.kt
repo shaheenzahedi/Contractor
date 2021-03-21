b39282a06ab477037e84a44ac12122c195f6e0d1
shaheenzahedi <shaheen.d2@gmail.com>
2021-03-21 13:42:19 +0330

    added mapper for contract

---
+++ src/main/kotlin/service/mapper/ContractMapper.kt
@@ -0,0 +1,23 @@
+package service.mapper
+
+
+import java.io.IOException
+import java.io.InputStreamReader
+import com.google.gson.Gson
+import domain.contract.ContractModel
+
+
+class ContractMapper {
+    fun getJson() {
+        try {
+            InputStreamReader(
+                this.javaClass.getResourceAsStream("/sample-contract.json")
+            ).use { reader ->
+                val result: ContractModel = Gson().fromJson(reader, ContractModel::class.java)
+                println(result.consumer.name) // prints "bat"
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+}
