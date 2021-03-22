7e86b84b455fcada612f353965ddc6e9bc9504c7
shaheenzahedi <shaheen.d2@gmail.com>
2021-03-22 17:45:00 +0430

    added jpoem and kpoem and created some sample test class with it

--- src/main/kotlin/presentation/main.kt
+++ src/main/kotlin/presentation/main.kt
@@ -1,11 +1,55 @@
 package presentation
 
-import domain.contract.ContractModel
-import service.mapper.JsonMapper
+import com.squareup.javapoet.AnnotationSpec
+import com.squareup.javapoet.ClassName
+import com.squareup.javapoet.FieldSpec
+import com.squareup.javapoet.TypeSpec
+import javax.lang.model.element.Modifier
+
 
 fun main(args: Array<String>) {
-    val x = JsonMapper<ContractModel>()
-        .getJson(ContractModel::class.java,
-            "src/main/resources/contracts/sample-contract1.json")
-    println(x.interactions)
+//    val contractModel = JsonMapper<ContractModel>()
+//        .getJson(
+//            ContractModel::class.java,
+//            "src/main/resources/contracts/sample-contract6.json"
+//        )
+    val x = TypeSpec.classBuilder("SampleIntegrationTest")
+        .addAnnotation(
+            AnnotationSpec
+                .builder(ClassName.get("org.springframework.boot.test.context", "SpringBootTest"))
+                .addMember("classes", "[MooverApp::class]")
+                .build()
+        )
+        .addAnnotation(
+            AnnotationSpec
+                .builder(
+                    ClassName.get("org.springframework.boot.test.autoconfigure.web.servlet", "AutoConfigureMockMvc")
+                )
+                .build()
+        )
+        .addAnnotation(
+            AnnotationSpec
+                .builder(ClassName.get("org.springframework.security.test.context.support", "WithMockUser"))
+                .build()
+        )
+        .addAnnotation(
+            AnnotationSpec
+                .builder(ClassName.get("org.junit.jupiter.api.extension", "Extensions"))
+                .addMember("value","ExtendWith(MockitoExtension::class)")
+                .build()
+        )
+        .addField(
+            FieldSpec
+                .builder(ClassName.get("com.example","SampleRepository"),"fooRepo")
+                .addAnnotation(
+                    AnnotationSpec
+                        .builder(ClassName.get("org.springframework.beans.factory.annotation", "Autowired"))
+                        .build()
+                )
+                .addModifiers(Modifier.PRIVATE,Modifier.STATIC,Modifier.FINAL)
+                .build()
+        )
+        .build()
+
+    println(x.toString())
 }
--- src/main/kotlin/service/mapper/JsonMapper.kt
+++ src/main/kotlin/service/mapper/JsonMapper.kt
@@ -7,6 +7,6 @@ import java.io.File
 
 class JsonMapper<T> {
     fun getJson(clazz: Class<T>, path: String): T = path.asResource { Gson().fromJson(it, clazz) }
-    private fun String.asResource(work: (String) -> T) = work(getResourceAsText(this))
+    private inline fun String.asResource(work: (String) -> T) = work(getResourceAsText(this))
     private fun getResourceAsText(path: String) = File(path).readText()
 }
