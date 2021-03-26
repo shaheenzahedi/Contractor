682df70825a50d3c2488f0423aa94e107c464848
shaheenzahedi <shaheen.d2@gmail.com>
2021-03-26 12:13:12 +0430

    renamed parent dir

--- src/main/kotlin/service/generators/fields/JFieldGenerator.kt
+++ src/main/kotlin/service/generators/fields/JFieldGenerator.kt
@@ -8,10 +8,8 @@ import javax.lang.model.element.Modifier
 class JFieldGenerator(
     private val annotationGenerator: IAnnotationGenerator
 ) : IFieldGenerator {
-    override fun repositoryField(): FieldSpec.Builder {
-        return FieldSpec
-            .builder(ClassName.get("com.example", "SampleRepository"), "fooRepo")
-            .addAnnotation(annotationGenerator.autowiredAnnotation.build())
-            .addModifiers(Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
-    }
+    override fun repositoryField(): FieldSpec.Builder = FieldSpec
+        .builder(ClassName.get("com.example", "SampleRepository"), "fooRepo")
+        .addAnnotation(annotationGenerator.autowiredAnnotation.build())
+        .addModifiers(Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
 }
