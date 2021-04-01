55c4d633cc2172900975a82fb5d64ba7d7b4055e
shaheenzahedi <shaheen.d2@gmail.com>
2021-04-01 14:10:56 +0430

    added setup method

--- src/main/kotlin/service/generators/methods/JMethodGenerator.kt
+++ src/main/kotlin/service/generators/methods/JMethodGenerator.kt
@@ -7,9 +7,7 @@ class JMethodGenerator(
     private val annotationGenerator: JAnnotationGenerator
 ) : MethodGenerator {
     override fun getAllMethod(): MethodSpec.Builder {
-        return MethodSpec.methodBuilder("getAllBookmarks").addStatement("int sum = 0")
-            .beginControlFlow("for (int i = 0; i <= 10; i++)")
-            .addStatement("sum += i")
-            .endControlFlow()
+        return MethodSpec.methodBuilder("setup")
+            .addAnnotation(annotationGenerator.beforeEachAnnotation.build())
     }
 }
