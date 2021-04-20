20a2cc25776b67ae0d9369b9b37f9fd2e5bcf3d7
shaheenzahedi <shaheen.d2@gmail.com>
2021-04-20 10:23:40 +0430

    attempt to write mapping class made

---
+++ src/main/kotlin/service/mapper/ContractMapper.kt
@@ -0,0 +1,14 @@
+package service.mapper
+
+import domain.RTTest.RTTest
+import domain.contract.ContractModel
+
+class ContractMapper(model: ContractModel) {
+    fun extreactRTTEst(){
+        TODO("Mapping should be more studied as it is complicated")
+//        return RTTest(
+//
+//        )
+    }
+}
+
