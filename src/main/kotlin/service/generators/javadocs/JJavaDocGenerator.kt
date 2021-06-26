a8db2ce0bf37b5583aca3f197d0067dfa6888a27
shaheenzahedi <shaheen.d2@gmail.com>
2021-06-26 19:19:58 +0430

    refactored java doc generator

--- src/main/kotlin/service/generators/javadocs/JJavaDocGenerator.kt
+++ src/main/kotlin/service/generators/javadocs/JJavaDocGenerator.kt
@@ -4,38 +4,46 @@ import com.squareup.javapoet.CodeBlock
 import domain.RTTest.ReadyToTestModel
 
 class JJavaDocGenerator : JavaDocGenerator {
+    private fun makeCodeBlock(commentLines: List<String>): CodeBlock {
+        return CodeBlock.builder().apply { commentLines.forEach { add(it) } }.build()
+    }
+
     override fun statusTestJavaDocGenerator(model: ReadyToTestModel): CodeBlock {
-        return CodeBlock.builder()
-            .add("`${model.method?.name}\t${model.path}\n\n")
-            .add("Asserts if the status equals to ${model.status}")
-            .build()
+        return makeCodeBlock(
+            listOf(
+                "`${model.method?.name}\t${model.path}\n\n",
+                "Asserts if the status equals to ${model.status}"
+            )
+        )
     }
 
     override fun testClassJavaDocGenerator(): CodeBlock {
-        return CodeBlock.builder()
-            .add("BDD tests generated from the contracts from consumer-side\n\n")
-            .build()
+        return makeCodeBlock(
+            listOf("BDD tests generated from the contracts from consumer-side\n\n")
+        )
     }
 
     override fun headerTestJavaDocGenerator(model: ReadyToTestModel): CodeBlock {
-        return CodeBlock.builder()
-            .add("`${model.method?.name}\t${model.path}\n\n")
-            .add("Asserts that the response has the desired header\n")
-            .add("Asserts if the response header matches to what we defined in the contract\n")
-            .build()
+        return makeCodeBlock(
+            listOf(
+                "`${model.method?.name}\t${model.path}\n\n",
+                "Asserts that the response has the desired header\n",
+                "Asserts if the response header matches to what we defined in the contract\n"
+            )
+        )
     }
 
     override fun bodyTestJavaDocGenerator(model: ReadyToTestModel): CodeBlock {
-        return CodeBlock.builder()
-            .add("`${model.method?.name}\t${model.path}\n\n")
-            .add("Asserts if the Json is what we expect in the contract")
-            .build()
+        return makeCodeBlock(
+            listOf(
+                "`${model.method?.name}\t${model.path}\n\n",
+                "Asserts if the Json is what we expect in the contract"
+            )
+        )
     }
 
     override fun setupTestJavaDocGenerator(): CodeBlock {
-        return CodeBlock.builder()
-            .add("Retrieves the desired Json and put it into an entity model")
-            .build()
+        return makeCodeBlock(listOf("Retrieves the desired Json and put it into an entity model"))
     }
 
 }
