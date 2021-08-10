97c18dbfbaf18f399e2e089f31f5d298120dbefd
shaheenzahedi <shaheen.d2@gmail.com>
2021-08-10 09:30:12 +0430

    set up a very basic look on the form

--- contractor-front-end/src/App.js
+++ contractor-front-end/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <h1>General</h1>
+            <label htmlFor='http-method'>Http Method</label>
+            <input id='http-method'/><br/>
+            <label htmlFor='path'>Path</label>
+            <input id='path'/><br/>
+            <label htmlFor='status'>Status</label>
+            <input id='status'/>
+            <h1>Request</h1>
+            <label htmlFor='request-header'>Header</label>
+            <input id='request-header'/><br/>
+            <label htmlFor='request-body'>Body</label>
+            <input id='request-body'/><br/>
+            <label htmlFor='request-param-rules'>Param Rules</label>
+            <input id='request-param-rules'/>
+            <h1>Response</h1>
+            <label htmlFor='request-header'>Header</label>
+            <input id='request-header'/><br/>
+            <label htmlFor='request-header-rules'>Header Rules</label>
+            <input id='request-header'/><br/>
+            <label htmlFor='request-body'>Body</label>
+            <input id='request-body'/><br/>
+            <label htmlFor='request-body-rules'>Body Rules</label>
+            <input id='request-body'/><br/><br/>
+            <button>Export</button>
+            <button>Import</button>
+        </>
+    );
 }
 
 export default App;
