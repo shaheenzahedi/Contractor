5223c5c19409c9139df6aee38a344bca60d5a848
shaheenzahedi <shaheen.d2@gmail.com>
2021-08-18 19:16:41 +0430

    added khttp

--- Contractor-Provider-Side/build.gradle.kts
+++ Contractor-Provider-Side/build.gradle.kts
@@ -6,27 +6,22 @@ plugins {
 }
 
 val koinVersion: String = "3.0.1"
-repositories {
-    mavenCentral()
+allprojects {
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
 }
-//val compileKotlin: KotlinCompile by tasks
-//compileKotlin.kotlinOptions {
-//    jvmTarget = "1.8"
-//}
-//val compileTestKotlin: KotlinCompile by tasks
-//compileTestKotlin.kotlinOptions {
-//    jvmTarget = "1.8"
-//}
+
 dependencies {
     implementation("com.google.code.gson:gson:2.8.6")
     implementation("com.squareup:javapoet:1.13.0")
     implementation("com.squareup:kotlinpoet:1.8.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.11.3")
 
+    implementation("com.github.jkcclemens:khttp:0.1.0")
     implementation("io.insert-koin:koin-core:$koinVersion")
     implementation("io.insert-koin:koin-core-ext:$koinVersion")
-//    testImplementation("org.koin:koin-test:$koinVersion")
-//    testImplementation("org.koin:koin-test-junit4:$koinVersion")
     testImplementation("io.insert-koin:koin-test-junit5:$koinVersion")
     implementation(kotlin("stdlib-jdk8"))
 }
@@ -44,26 +39,3 @@ version = "1.0-SNAPSHOT"
 tasks.test {
     useJUnitPlatform()
 }
-//
-//tasks.withType<KotlinCompile>() {
-//    kotlinOptions.jvmTarget = "1.8"
-//}
-//
-//application {
-//    mainClass.set("MainKt")
-//}
-//tasks.withType<Jar> {
-//    manifest {
-//        attributes["Main-Class"] = "MainKt"
-//    }
-//}
-//tasks.withType<KotlinCompile> {
-//    kotlinOptions {
-//        freeCompilerArgs = listOf("-Xjsr305=strict")
-//        jvmTarget = "1.8"
-//    }
-//
-//    tasks.withType<Test> {
-//        useJUnitPlatform()
-//    }
-//}
--- Contractor-Provider-Side/settings.gradle.kts
+++ Contractor-Provider-Side/settings.gradle.kts
@@ -1,3 +1,3 @@
 
-rootProject.name = "Contractor"
+rootProject.name = "contractor-provider"
 
