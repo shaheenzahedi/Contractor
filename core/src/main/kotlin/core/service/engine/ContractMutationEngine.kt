6a768a455250d143ab7a6cc6a6023b5faf9eed52
shaheenzahedi <shaheen.d2@gmail.com>
2021-10-14 19:54:08 +0330

    mutants are created one by one

--- core/src/main/kotlin/core/service/engine/ContractMutationEngine.kt
+++ core/src/main/kotlin/core/service/engine/ContractMutationEngine.kt
@@ -6,65 +6,139 @@ import com.natpryce.snodge.json.forStrings
 import com.natpryce.snodge.mutants
 import core.domain.ready_to_generate.HTTPMethod
 import core.domain.ready_to_generate.ReadyToTestModel
-import core.domain.ready_to_generate.request.ReadyRequestModel
-import core.domain.ready_to_generate.response.ReadyResponseModel
 import kotlin.random.Random
 
 class ContractMutationEngine(private val contracts: List<ReadyToTestModel>?) {
     private val mutationCount = 5
 
     fun generateContractMutants(): List<ReadyToTestModel>? {
-        return contracts?.mapIndexed { index, contract ->
-            val statusMutations = generateStatusMutation()
-            val methodMutations = generateMethodMutations()
-            val bodyResponseMutations = generateResponseBodyMutations(index)
-            val requestBodyMutations = generateRequestBodyMutations(index)
-            val requestHeaderMutations = generateRequestHeaderMutations(index)
-            val paramsMutations = generateParamsMutations(index)
-            val cookiesMutations = generateCookiesMutations(index)
-            (0 until mutationCount).map { i ->
-              ReadyToTestModel(
-                    baseUrl = contract.baseUrl,
-                    port = contract.port,
-                    method = methodMutations[i],
-                    path = contract.path,
-                    status= statusMutations[i],
-                    request = ReadyRequestModel(
-                        body = requestBodyMutations?.get(i),
-                        headers = requestHeaderMutations?.get(i),
-                        params = paramsMutations?.get(i),
-                        cookies = cookiesMutations?.get(i),
-                        data = null
-                    ),
-                    response = ReadyResponseModel(
-                        body = bodyResponseMutations?.get(i),
-                        headers = null,
-                        bodyPredicates = null,
-                        headerPredicates = null
-                    )
+        return contracts?.flatMapIndexed { index, contract ->
+            val statusMutations = generateStatusMutation(contract.status!!).map { contract.copy(status = it) }
+            val methodMutations = generateMethodMutations(contract.method).map { contract.copy(method = it) }
+            val bodyResponseMutations = generateResponseBodyMutations(index, contract.response?.body)?.map {
+                contract.copy(response = contract.response?.copy(body = it))
+            }
+            val requestBodyMutations = generateRequestBodyMutations(index, contract.request?.body)?.map {
+                contract.copy(request = contract.request?.copy(body = it))
+            }
+            val requestHeaderMutations = generateRequestHeaderMutations(index, contract.request?.headers)?.map {
+                contract.copy(request = contract.request?.copy(headers = it))
+            }
+            val paramsMutations = generateParamsMutations(index, contract.request?.params)?.map {
+                contract.copy(
+                    request = contract.request?.copy(params = it)
                 )
             }
-        }?.toMutableList()?.flatten()
+            val cookiesMutations = generateCookiesMutations(index, contract.request?.cookies)?.map {
+                contract.copy(
+                    request = contract.request?.copy(cookies = it)
+                )
+            }
+            mutableListOf<ReadyToTestModel>().apply {
+                addAll(statusMutations)
+                addAll(methodMutations)
+                if (bodyResponseMutations != null) addAll(bodyResponseMutations)
+                if (requestBodyMutations != null) addAll(requestBodyMutations)
+                if (requestHeaderMutations != null) addAll(requestHeaderMutations)
+                if (paramsMutations != null) addAll(paramsMutations)
+                if (cookiesMutations != null) addAll(cookiesMutations)
+            }
+        }
     }
 
-    fun generateStatusMutation() = (1..mutationCount).map { (100..999).random() }
+    fun generateStatusMutation(status: Int) = mutableListOf(
+        100,        //Continue
+        101,        //Switching Protocol
+        300,         //Multiple Choices
+        301,        //Moved Permanently
+        302,        //Found
+        303,        //See Other
+        304,        //Not Modified
+        305,        //Use Proxy
+        306,        //Switch Proxy
+        307,        //Temporary Redirect
+        308,        //Permanent Redirect
+        500,        //Internal Server Error
+        501,        //Not Implemented
+        502,        //Bad Gateway
+        503,        //Service Unavailable
+        504,        //Gateway Timeout
+        505,        //HTTP Version Not Supported
+        506,        //Variant Also Negotiates
+        510,        //Not Extended
+        511,        //Network Authentication Required
+        200,        //OK
+        201,        //Created
+        202,        //Accepted
+        203,        //Non-Authoritive Information
+        204,        //No Content
+        205,        //Reset Content
+        206,        //Partial Content
+        226,        //IM Used
+        400,        //Bad Request
+        401,        //Unauthorized
+        402,        //Payment Required
+        403,        //Forbidden
+        404,        //Not Found
+        405,        //Method Not Allowed
+        406,        //Not Acceptable
+        407,        //Proxy Authentication Required
+        408,        //Request Timeout
+        409,        //Conflict
+        410,        //Gone
+        411,        //Length Required
+        412,        //Precondition Failed
+        413,        //Payload Too Large
+        414,        //URI Too Long
+        415,        //Unsupported Media Type
+        416,        //Range Not Satisfiable
+        417,        //Expectation Failed
+        418,        //I’m a teapot
+        421,        //Misdirected Request
+        426,        //Upgrade Required
+        428,        //Precondition Required
+        429,        //Too Many Requests
+        431,        //Request Header Fields Too Large
+        451,        //Unavailable For Legal Reasons
+    ).apply { removeIf { it == status } }
 
-    fun generateMethodMutations() = HTTPMethod.values().toMutableList()
+    fun generateMethodMutations(method: HTTPMethod) =
+        HTTPMethod.values().toMutableList().apply { removeIf { it == method } }
 
-    fun generateResponseBodyMutations(position: Int) =
-        generateAnyMutatedPairs(contracts?.get(position)?.response?.body)
+    fun generateResponseBodyMutations(
+        position: Int,
+        body: LinkedHashMap<String, Any>?
+    ): List<LinkedHashMap<String, Any>>? {
+        return generateAnyMutatedPairs(contracts?.get(position)?.response?.body)?.filter { it != body }
+    }
 
-    fun generateRequestBodyMutations(position: Int) =
-        generateAnyMutatedPairs(contracts?.get(position)?.request?.body)
+    fun generateRequestBodyMutations(
+        position: Int,
+        body: LinkedHashMap<String, Any>?
+    ): List<LinkedHashMap<String, Any>>? {
+        return generateAnyMutatedPairs(contracts?.get(position)?.request?.body)?.filter { it != body }
+    }
 
-    fun generateRequestHeaderMutations(position: Int) =
-        generateStringMutatedPairs(contracts?.get(position)?.request?.headers)
+    fun generateRequestHeaderMutations(
+        position: Int,
+        headers: LinkedHashMap<String, String>?
+    ): List<LinkedHashMap<String, String>>? {
+        return generateStringMutatedPairs(contracts?.get(position)?.request?.headers)?.filter { it != headers }
+    }
 
-    fun generateParamsMutations(position: Int) =
-        generateStringMutatedPairs(contracts?.get(position)?.request?.params)
+    fun generateParamsMutations(
+        position: Int,
+        params: LinkedHashMap<String, String>?
+    ): List<LinkedHashMap<String, String>>? {
+        return generateStringMutatedPairs(contracts?.get(position)?.request?.params)?.filter { it != params }
+    }
 
-    fun generateCookiesMutations(position: Int) =
-        generateStringMutatedPairs(contracts?.get(position)?.request?.cookies)
+    fun generateCookiesMutations(
+        position: Int,
+        cookies: LinkedHashMap<String, String>?
+    ): List<LinkedHashMap<String, String>>? {
+        return generateStringMutatedPairs(contracts?.get(position)?.request?.cookies)?.filter { it != cookies }
+    }
 
     private fun generateStringMutatedPairs(body: LinkedHashMap<String, String>?) =
         body?.let { generateMutatedPairs(it).map { mutant -> mutant as LinkedHashMap<String, String> } }
