b43b654534df9044edef624bfc74a3fff41a21a6
shaheenzahedi <shaheen.d2@gmail.com>
2022-01-21 23:05:39 +0330

    adding pact contracts halfway done

---
+++ contract_samples/pact/evaluation/idealo.design/blogpost-provider-contract.spec.js
@@ -0,0 +1,91 @@
+import { Verifier } from "@pact-foundation/pact";
+import path from "path";
+import { app } from "./server";
+import { Blog } from "./models/Blog";
+
+import "@testing-library/jest-dom/extend-expect";
+import {
+  mockedArchivedBlogpost,
+  mockedBlogpost,
+  mockedUpdatedBlogpost,
+} from "../ui/pages/BlogPage/consumer-contract.spec";
+
+jest.mock("./models/Blog", () => {
+  return {
+    Blog: {
+      fetchAllBlogposts: jest.fn(),
+      updateBlogpost: jest.fn(),
+      archiveSingleBlogpost: jest.fn(),
+      fetchAllBlogpostsByCategorySlug: jest.fn(),
+      fetchAllCategories: jest.fn(),
+      fetchSingleBlogpost: jest.fn(),
+      deleteSingleBlogpost: jest.fn(),
+    }
+  };
+});
+
+jest.mock('./models/Library', () => {
+  return {
+    Library: {
+      fetchAllComponents: jest.fn(),
+      fetchSingleComponent: jest.fn(),
+    },
+    Tags: {
+      fetchTags: jest.fn()
+    }
+  }
+})
+
+const distinctCategories = {
+  categorydisplayvalue: "Testing Category",
+  categoryslug: "testing-category",
+};
+
+describe("Pact Verification", () => {
+  const server = app.listen(6666, "0000", () =>
+    console.log("server running for api testing")
+  );
+
+  test("should validate the expectations of our consumer", () => {
+    Blog.fetchAllBlogposts.mockReturnValue([mockedBlogpost, mockedBlogpost, mockedBlogpost]);
+    Blog.updateBlogpost.mockReturnValue(mockedUpdatedBlogpost);
+    Blog.fetchSingleBlogpost.mockReturnValue(mockedBlogpost);
+    Blog.fetchAllBlogpostsByCategorySlug.mockReturnValue(mockedBlogpost);
+    Blog.archiveSingleBlogpost.mockReturnValue(mockedArchivedBlogpost);
+    Blog.fetchAllCategories.mockReturnValue([
+      distinctCategories,
+      distinctCategories,
+      distinctCategories,
+      distinctCategories,
+    ]);
+    Blog.deleteSingleBlogpost.mockReturnValue("successfully deleted blogpost");
+
+    const opts = {
+      provider: "BlogTestingProvider",
+      providerBaseUrl: "http://localhost:6666",
+      // pactBrokerUrl: process.env.PACT_BROKER_URL,
+      //  pactBrokerToken: process.env.PACT_BROKER_TOKEN,
+      pactUrls: [
+        path.resolve(
+          process.cwd(),
+          "src/__tests__/pact/blogtestingconsumer-blogtestingprovider.json"
+        ),
+      ],
+      publishVerificationResult: true,
+      providerVersion: "1.0.0",
+      logLevel: "INFO",
+    };
+
+    return new Verifier(opts)
+      .verifyProvider()
+      .then((output) => {
+        server.close();
+        console.log("pact verification complete !");
+        console.log(output);
+      })
+      .catch((error) => {
+        server.close();
+        fail(error);
+      });
+  });
+});
---
+++ contract_samples/pact/evaluation/jest-pact-typescript/json.pacttest.ts
@@ -0,0 +1,22 @@
+import * as jestpact from "jest-pact";
+import * as supertest from "supertest";
+import * as interaction from "./expectation/json.expectation";
+import * as json from "./requestResponse/json.reqRes";
+
+jestpact.pactWith(
+  { consumer: "test-consumer", provider: "json-provider" },
+  async (provider: any) => {
+    const client = () => {
+      const url = `${provider.mockService.baseUrl}`;
+      return supertest(url);
+    };
+    test("should accept a valid get request to get a pet", async () => {
+      await provider.addInteraction(interaction.postValidRequest);
+
+      await client()
+        .get("/v2/pet/1845563262948980200")
+        .set("api_key", "[]")
+        .expect(200, json.getPetValidResponse);
+    });
+  }
+);
